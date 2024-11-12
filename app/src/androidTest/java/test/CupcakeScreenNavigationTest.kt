c6f5b5c2733e4dbd738f187c91b2ac103565449b
mehrozejalal <mjalal@uwaterloo.ca>
2024-11-12 13:46:16 -0500

    Set up the nav host

--- app/src/androidTest/java/test/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/test/CupcakeScreenNavigationTest.kt
@@ -1,4 +1,36 @@
 package test
 
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.cupcake.CupcakeApp
+import com.example.cupcake.ui.theme.CupcakeTheme
+import org.junit.Before
+import org.junit.Rule
+
 class CupcakeScreenNavigationTest {
+
+    @get: Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupCupcakeNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            CupcakeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    CupcakeApp(navController = navController)
+                }
+            }
+        }
+    }
+
 }
